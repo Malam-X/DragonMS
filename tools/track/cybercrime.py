f1beba7dda0fa9aedb0274497eda791e3bd2198d
DR4G0N5 <63049672+Malam-X@users.noreply.github.com>
2021-03-01 04:26:35 -0500

    Fix Bug

--- tools/track/cybercrime.py
+++ tools/track/cybercrime.py
@@ -3,11 +3,12 @@ import sys
 import json
 from etc.set import *
 
-query = ''
-if len(sys.argv) > 1:
-   query = sys.argv[1]
-x = (json.dumps(cybercrimeTrackerAPI().search(query), indent=2))
-NAME_PATH = 'result/track_result.txt'
-with open(f'{NAME_PATH}', 'w') as f:
-    f.write(x)
-print(f'{PULS} Success check /{NAME_PATH}')
+def main_track():
+   query = ''
+   if len(sys.argv) > 1:
+      query = sys.argv[1]
+   x = (json.dumps(cybercrimeTrackerAPI().search(query), indent=2))
+   NAME_PATH = 'result/track_result.txt'
+   with open(f'{NAME_PATH}', 'w') as f:
+       f.write(x)
+   print(f'{PULS} Success check /{NAME_PATH}')
