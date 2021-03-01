bfcb559fd40ad7aef857c3cce520fb9d198e0104
DR4G0N5 <63049672+Malam-X@users.noreply.github.com>
2021-03-01 04:21:20 -0500

    Add files via upload

--- etc/set.py
+++ etc/set.py
@@ -1,6 +1,9 @@
 #Module
+
 import sys,os,socket,time,datetime
+
 #Setting
+
 def clear():
     if sys.platform.startswith('linux'):
         os.system('clear')
@@ -8,6 +11,14 @@ def clear():
         os.system('clear')
     else:
         os.system('cls')
+
 promt_choice = '\u001b[1m[\u001b[32;1m+\u001b[0m\u001b[1m] Choice\u001b[0m: '
 promt_target = '\u001b[1m[\u001b[32;1m+\u001b[0m\u001b[1m] Target\u001b[0m: '
-private_ip = socket.gethostbyname(socket.gethostname())
+PULS         = '\u001b[1m[\u001b[32;1m+\u001b[0m\u001b[1m]\u001b[0m'
+MINS         = '\u001b[1m[\u001b[31m-\u001b[0m\u001b[1m]\u001b[0m'
+ERRORS       = '\u001b[1m[\u001b[31mERROR!\u001b[0m\u001b[1m]\u001b[0m'
+private_ip   = socket.gethostbyname(socket.gethostname())
+ERROR_SYM    = '[\u001b[31m✘\u001b[0m]'
+SUCES_SYM    = '[\u001b[32;1m✔\u001b[0m]'
+NFound       = '\n\u001b[1m[\u001b[31mNot Found Choice!\u001b[0m\u001b[1m]\u001b[0m'
+Aborted      = '\n\u001b[1m[\u001b[31mABORTED!\u001b[0m\u001b[1m]\u001b[0m'
